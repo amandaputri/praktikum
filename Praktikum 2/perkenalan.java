c27b93b214c9ca36df43466c611403fe6787822b
Amanda <amandaputri@student.telkomuniversity.ac.id>
2020-09-20 16:12:26 +0700

    perkenalan.java

---
+++ Praktikum 2/perkenalan.java
@@ -0,0 +1,7 @@
+public class perkenalan {
+    public static void main(String [] args) {
+		System.out.println("Nama:Amanda Putri!!");
+		System.out.println("NIM:6706202036!!");
+		System.out.println("Kelas:D3IF44-02!!");
+    }
+}
